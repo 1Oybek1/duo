18a40c4c70a2739835bf9d4120f0e12a1a4cbf5f
Doniyor <doniyormurodov246810@gmail.com>
2024-09-09 23:54:40 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import CenterContainer from "./Components/Center/Center"
 import Navbar from "./Components/Navbar/Navbar"
+import Section from "./Components/Section/Section"
 
 function App() {
 
 
   return (
     <>
-      <Navbar/>
-      <Section/>
-      <CenterContainer />
+<Navbar/>
+<Section/>
+<CenterContainer/>
     </>
   )
 }
