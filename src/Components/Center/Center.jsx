3ffc951aa893827933e03d09d7953523212c4b55
Oybek <oybek12093487@gmail.com>
2024-09-09 18:36:42 +0500

    center qo'shdim

--- src/Components/Center/Center.jsx
+++ src/Components/Center/Center.jsx
@@ -5,7 +5,9 @@ import './Center.css'
 const CenterContainer = ()=> {
     return(
         <div className="center">
-            <h5 style={{textAlign:"center"}}>Our Portfolio</h5>
+            <h5 className="h text-white mt-6 ">Our Portfolio</h5>
+            <h1>What do we do</h1>
+
         </div>
     )
 }
