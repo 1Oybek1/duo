14880bf71e690d335e8c38f5bfa0a4015a80b6e4
Oybek <oybek12093487@gmail.com>
2024-09-09 21:38:16 +0500

    .

--- src/Components/Center/Center.jsx
+++ src/Components/Center/Center.jsx
@@ -5,12 +5,44 @@ import './Center.css'
 const CenterContainer = ()=> {
     return(
         <div className="center text-center">
-            <div className="">
-                <h5 className="h text-white ">Our Portfolio</h5>
+            <div className="" style={{marginTop:"60px",}}>
+                <h1 className="h text-white" style={{ fontSize:"40px"}}>What do we do</h1>
+                <p className="text-white">all projects that we have already done , proven can help to use more <br /> comfortable, then can used by client from our business</p>
             </div>
-            <div className="">
-                <h1 className="h text-white mt-5">What do we do</h1>
+            <div class="main" style="margin-top: 100px;">
+        <ul class="cards">
+          <li class="cards_item">
+            <div class="card1">
+              <div class="card_image"></div>
             </div>
+          </li>
+          <li class="cards_item">
+            <div class="card1">
+              <div class="card_image"></div>
+            </div>
+          </li>
+          <li class="cards_item">
+            <div class="card1">
+              <div class="card_image"></div>
+            </div>
+          </li>
+          <li class="cards_item">
+            <div class="card1">
+              <div class="card_image"></div>
+            </div>
+          </li>
+          <li class="cards_item">
+            <div class="card1">
+              <div class="card_image"></div>
+            </div>
+          </li>
+          <li class="cards_item">
+            <div class="card1">
+              <div class="card_image"></div>
+            </div>
+          </li>
+        </ul>
+    </div>
         </div>
     )
 }
