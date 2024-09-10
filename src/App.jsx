af355e8c33cd0e0081247802a19e005d2c8ffd08
Oybek <oybek12093487@gmail.com>
2024-09-10 14:58:22 +0500

    xatolik

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,17 @@ function App() {
 
 
   return (
-<<<<<<< HEAD
-    <>0
+
       <Navbar/>
       <Section/>
       <CenterContainer />
-=======
+
     <>
 <Navbar/>
 <Section/>
 <CenterContainer/>
->>>>>>> 4d618652b8f74a04564c77de07db3ef2bf149048
-    </>
+
   )
 }
 
-export default App
-
-
+export default App;
--- src/Components/Center/Center.jsx
+++ src/Components/Center/Center.jsx
@@ -5,44 +5,7 @@ import './Center.css'
 const CenterContainer = ()=> {
     return(
         <div className="center text-center">
-            <div className="" style={{marginTop:"60px",}}>
-                <h1 className="h text-white" style={{ fontSize:"40px"}}>What do we do</h1>
-                <p className="text-white">all projects that we have already done , proven can help to use more <br /> comfortable, then can used by client from our business</p>
-            </div>
-            <div class="main" style="margin-top: 100px;">
-        <ul class="cards">
-          <li class="cards_item">
-            <div class="card1">
-              <div class="card_image"></div>
-            </div>
-          </li>
-          <li class="cards_item">
-            <div class="card1">
-              <div class="card_image"></div>
-            </div>
-          </li>
-          <li class="cards_item">
-            <div class="card1">
-              <div class="card_image"></div>
-            </div>
-          </li>
-          <li class="cards_item">
-            <div class="card1">
-              <div class="card_image"></div>
-            </div>
-          </li>
-          <li class="cards_item">
-            <div class="card1">
-              <div class="card_image"></div>
-            </div>
-          </li>
-          <li class="cards_item">
-            <div class="card1">
-              <div class="card_image"></div>
-            </div>
-          </li>
-        </ul>
-    </div>
+           
         </div>
     )
 }
