a654f849c50f092a474baf99487ad212b066e3d2
Oybek <oybek12093487@gmail.com>
2024-09-12 00:45:58 +0500

    footer oxshamadi

--- src/Components/Center/Center.jsx
+++ src/Components/Center/Center.jsx
@@ -4,6 +4,19 @@ import phone from '../../assets/phone.jpg'
 import cloud from '../../assets/cloud.png'
 import fone from '../../assets/fone.png'
 import coding from '../../assets/coding.jpg'
+import { Card } from "flowbite-react";
+import { Button } from "flowbite-react";
+import { Footer } from "flowbite-react";
+import {
+    FooterBrand,
+    FooterCopyright,
+    FooterDivider,
+    FooterIcon,
+    FooterLink,
+    FooterLinkGroup,
+    FooterTitle,
+  } from "flowbite-react";
+  import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 
 
 
@@ -97,7 +110,57 @@ const CenterContainer = ()=> {
                 </div>
             </div>
 
-           
+            <Footer bgDark>
+      <div className="w-full">
+        <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
+          <div>
+            <Footer.Title title="Company" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">About</Footer.Link>
+              <Footer.Link href="#">Careers</Footer.Link>
+              <Footer.Link href="#">Brand Center</Footer.Link>
+              <Footer.Link href="#">Blog</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="help center" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">Discord Server</Footer.Link>
+              <Footer.Link href="#">Twitter</Footer.Link>
+              <Footer.Link href="#">Facebook</Footer.Link>
+              <Footer.Link href="#">Contact Us</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="legal" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">Privacy Policy</Footer.Link>
+              <Footer.Link href="#">Licensing</Footer.Link>
+              <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="download" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">iOS</Footer.Link>
+              <Footer.Link href="#">Android</Footer.Link>
+              <Footer.Link href="#">Windows</Footer.Link>
+              <Footer.Link href="#">MacOS</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+        </div>
+        <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
 
         </div>
     )
