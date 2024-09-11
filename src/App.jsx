f3c4a8861414a0bc006d254be14fb0854ccd043e
Doniyor <doniyormurodov246810@gmail.com>
2024-09-11 20:25:51 +0500

    app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./Components/Navbar/Navbar"
+import Section from "./Components/Section/Section"
+import CenterContainer from "./Components/Center/Center";
+
+function App() {
+
+
+  return (
+    <>
+<Navbar/>
+<Section/>
+<CenterContainer/>
+
+    </>
+  )
+}
+
+export default App;
